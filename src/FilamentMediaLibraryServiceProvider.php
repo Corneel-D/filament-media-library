10fbc1b3ac3bc414f8d12897d5baa3d008f0c4cc
Corneel-D <Corneel-D@users.noreply.github.com>
2024-07-09 20:59:41 +0000

    Fix styling

--- src/FilamentMediaLibraryServiceProvider.php
+++ src/FilamentMediaLibraryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace CorneelD\FilamentMediaLibrary;
 
+use CorneelD\FilamentMediaLibrary\Commands\FilamentMediaLibraryCommand;
+use CorneelD\FilamentMediaLibrary\Testing\TestsFilamentMediaLibrary;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use CorneelD\FilamentMediaLibrary\Commands\FilamentMediaLibraryCommand;
-use CorneelD\FilamentMediaLibrary\Testing\TestsFilamentMediaLibrary;
 
 class FilamentMediaLibraryServiceProvider extends PackageServiceProvider
 {
